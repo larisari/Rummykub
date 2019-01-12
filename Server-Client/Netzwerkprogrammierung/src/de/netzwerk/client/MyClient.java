c1d3fae1fecb7bf2d7ad0016cbdc2acb30122321
markus <markus.franz@campus.lmu.de>
2019-01-12 12:35:45 +0100

    pseudo code with some ideas. added new methods/ constructors.

--- Server-Client/Netzwerkprogrammierung/src/de/netzwerk/client/MyClient.java
+++ Server-Client/Netzwerkprogrammierung/src/de/netzwerk/client/MyClient.java
@@ -1,5 +1,6 @@
 package de.netzwerk.client;
 
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.Serializable;
@@ -8,51 +9,124 @@ import java.net.Socket;
 import java.net.SocketAddress;
 
 public class MyClient implements Serializable {
-  public static void main(String[] args) {
-    //if ( neues Spiel gehostet ...
-      MyClient client = new MyClient("localhost", 48410); // Hostaddresse muss geändert werden, localhost ist immer eigener Rechner
-      client.sendMessage("Client verbunden");
+  private SocketAddress address; // auf dieser Addresse verbindet sich später der Client
+  private Socket socket;
+  private int port;
+  private int id;
 
-      // Spielbeitritt
-      //else
-      //MyClient remoteClient = new MyClient(server.getLocalHost(), 48410);
+
+  public Socket getSocket() {
+    return socket;
   }
 
-
-   private SocketAddress address; // auf dieser Addresse verbindet sich später der Client
-
-  public MyClient(String hostname, int port){
-      address = new InetSocketAddress(hostname, port); //hostname entspricht dem Rechner auf dem der Server läuft
+  public void setSocket(Socket socket) {
+    this.socket = socket;
   }
 
-
-
-  //diese Message wird nachher an den Server gesendet
-  public void sendMessage(String msg) {
-
-
-      try{
-          System.out.println("[Client] Verbinde zu Server....");
-           Socket socket = new Socket();
-          socket.connect(address,5000);
-          System.out.println("[Client] Verbunden!");
-
-
-          //nun müssen wir Daten schreiben
-
-          System.out.println("[Client] Sende Nachricht....");
-          PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-          pw.println(msg);
-          pw.flush(); // Daten dann abrauschen durch die Leitung
-          System.out.println("[Client] Nachricht gesendet!");
-
-
-          //Verbindung wieder schließen
-          //pw.close();
-          //socket.close();
-
-      }catch (Exception e) {
-          e.printStackTrace();
-      }
+  public int getId() {
+    return id;
   }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  /**
+   * Constructor
+   *
+   * @param hostname (IP-Adresse des Servers)
+   * @param port (Port, mit dem man den Server erreicht)
+   */
+  public MyClient(String hostname, int port, int id, Socket socket) {
+
+    address =
+        new InetSocketAddress(
+            hostname, port); // hostname entspricht dem Rechner auf dem der Server läuft
+    this.id = id;
+    this.socket = socket;
+  }
+
+//  /**
+//   * main-Methode, der einen Client initialisiert und eine Nachricht an den Server sendet.
+//   *
+//   * @param args
+//   */
+//  public static void main(String[] args) {
+//    // if ( neues Spiel gehostet ...
+//    MyClient client =
+//        new MyClient(
+//            "localhost",
+//            48410); // Hostaddresse muss geändert werden, localhost ist immer eigener Rechner
+//    client.sendMessage("Client sagt: Hallo Server!");
+//
+//    // Spielbeitritt
+//    // else
+//    // MyClient remoteClient = new MyClient(server.getLocalHost(), 48410);
+//  }
+//
+//  /**
+//   * Methode, zum Senden einer Nachricht an den Server
+//   *
+//   * @param msg message, die an den Server geschickt wird
+//   */
+//  public void sendMessage(String msg) {
+//
+//    try {
+//      System.out.println("[Client] Verbinde zu Server....");
+//      Socket socket = new Socket();
+//      socket.connect(address, 5000);
+//      System.out.println("[Client] Verbunden!");
+//
+//      // nun müssen wir Daten schreiben
+//
+//      System.out.println("[Client] Sende Nachricht....");
+//      PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+//      pw.println(msg);
+//      pw.flush(); // Daten dann abrauschen durch die Leitung
+//      System.out.println("[Client] Nachricht gesendet!");
+//
+//      // Verbindung wieder schließen
+//      pw.close();
+//      socket.close();
+//
+//    } catch (Exception e) {
+//      e.printStackTrace();
+//    }
+//  }
+//
+//
+//
+//
+//
+//  public void connectToOwnServer() {
+//    client = new MyClient("localhost", 48410);
+//    socket = new Socket();
+//    try {
+//      socket.connect(address, 5000);
+//    } catch (IOException e) {
+//      e.printStackTrace();
+//    }
+//  }
+//
+//  public void joinToOtherServer(SocketAddress serverAdress) {
+//    client = new MyClient("IP-Adresse über Getter aus Serverklasse holen", 48410);
+//    socket = new Socket();
+//    try {
+//      socket.connect(serverAdress);
+//    } catch (IOException e) {
+//      e.printStackTrace();
+//    }
+//  }
+//
+//  public void disconnect() {
+//    try {
+//      socket.close();
+//    } catch (IOException e) {
+//      e.printStackTrace();
+//    }
+//  }
+//
+//  public boolean isConnected() {
+//    return !socket.isClosed();
+//  }
 }
--- Server-Client/Netzwerkprogrammierung/src/de/netzwerk/server/MyServer.java
+++ Server-Client/Netzwerkprogrammierung/src/de/netzwerk/server/MyServer.java
@@ -1,78 +1,189 @@
 package de.netzwerk.server;
 
-import java.io.BufferedReader;
+import de.netzwerk.client.MyClient;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.Serializable;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class MyServer implements Serializable {
 
+  //Attribute
+
+  private ServerSocket serverSocket;
   private int port;
-  private boolean isRunning = true;
-  // Konstruktor braucht einen Port
-  public MyServer(int port) {
+  private ArrayList<MyClient> clients;
+  private Thread listening;
+  private boolean safeConnection; //soll alle Clients Verbindung halten (in Liste gespeichert werden)
+//  private boolean secureMode;
+//  private boolean stopped;
+
+  int clientCounter;
+
+
+
+
+  //Konstruktoren
+
+  public MyServer(int port, boolean safeConnection) {
+    this.clients = new ArrayList<MyClient>();
     this.port = port;
+    this.safeConnection = safeConnection;
+
+    preStart();
+
   }
 
-  public static void main(String[] args) {
-    MyServer server = new MyServer(48410);
-    server.startListening(); // damit wird Thread gestartet
+  //Methoden
+
+
+  /**
+   * all Server activities before server starts.
+   */
+  public void preStart() {
+    MyServer server = new MyServer(48410, true);
+    try{
+      serverSocket = new ServerSocket(48410);
+      if(clientCounter < 4) {
+        registerClient();
+      } else {
+        System.out.println("Sorry, es sind bereits 4 Spieler connected... Spiel starten Server!");
+      }
+
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
-  // darauf starten wir eigenen Thread -> Grund: ist kein eigenes Programm, sondern in unserem Spiel
-  // soll Server im Hintergrund laufen
+  private void registerClient(){
+    try{
+      Socket remoteClient = serverSocket.accept();
+      if(remoteClient.isConnected()) {
+        MyClient client = new MyClient("localhost", 48410, clients.size()+1, remoteClient); //
+
+        clients.add(client);
+        clientCounter++;
+        }
+    }catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+
+
+  /**
+   * listening to all open Threads and react to inquiries.
+   */
   public void startListening() {
-    new Thread(
-            new Runnable() {
-              @Override
-              public void run() {
 
-                while (isRunning) {
-
-                  try {
-                    System.out.println("[Server] Server starten.....");
-                    ServerSocket serverSocket = new ServerSocket(port); // = Server
-
-                    // Socket = Schnittstelle zum Client
-
-                    System.out.println("[Server] Warten auf Verbindung....");
-
-                    Socket remoteClient = serverSocket.accept(); // entfernter Client
-                    // an dieser Stelle wird das Programm solange geblockt, bis ein ClientSocket
-                    //                                 sich an dem ServerPort anmeldet
-                    System.out.println(
-                        "[Server] Client verbunden: "
-                            + remoteClient
-                                .getRemoteSocketAddress()); // damit wir sehen, woher Client sich
-                    //                                 verbunden hat
-                    // hier wird Verbindung dann verarbeitet
-
-                    // Input, OutputStream aus anderem Programm besser, nur zum Verständnis
-                    Scanner scanner =
-                        new Scanner(
-                            new BufferedReader(
-                                new InputStreamReader(remoteClient.getInputStream())));
-                    if (scanner.hasNextLine()) {
-                      System.out.println("[Server] Message from Client: " + scanner.nextLine());
-                    }
-
-                    // Anfrage an Model weitergeben
-                    // Antwort an Client zurückgeben
-
-                    // Verbindung danach wieder schließen
-                     scanner.close();
-                     remoteClient.close();
-                     serverSocket.close();
-
-                  } catch (IOException e) {
-                    e.printStackTrace();
-                  }
-                }
-              }
-            })
-        .start();
   }
+
+  /**
+   *
+   * @param socket
+   * @param clientID
+   */
+  public void sendReply(Socket socket, int clientID) {
+
+  }
+
+  /**
+   *
+   * @param clientID
+   * @param DatapackageID
+   * @param datapackage
+   */
+  public void sendMessage(int clientID, String DatapackageID, Object datapackage){
+
+  }
+
+
+
+//  private InetAddress address;
+//
+//
+//  private boolean isRunning = true;
+//
+//  public MyServer(int port) {
+//    this.port = port;
+//  }
+//
+//
+//  public static void main(String[] args) {
+//    MyServer server = new MyServer(48410);
+//    server.startListening(); // damit wird Thread gestartet
+//  }
+//
+//  // darauf starten wir eigenen Thread -> Grund: ist kein eigenes Programm, sondern in unserem Spiel
+//  // soll Server im Hintergrund laufen
+//  public void startListening() {
+//    new Thread(
+//            new Runnable() {
+//              @Override
+//              public void run() {
+//
+//                while (isRunning) {
+//
+//
+//
+//                  try {
+//                    System.out.println("[Server] Server starten.....");
+//                    serverSocket = new ServerSocket(port); // = Server
+//                    address = serverSocket.getInetAddress();
+//
+//
+//                    System.out.println("Server Inet Adresse"+address.getHostAddress());
+//
+//                    // Socket = Schnittstelle zum Client
+//
+//                    System.out.println("[Server] Warten auf Verbindung....");
+//
+//                    Socket remoteClient = serverSocket.accept(); // entfernter Client
+//                    // an dieser Stelle wird das Programm solange geblockt, bis ein ClientSocket
+//                    //                                 sich an dem ServerPort anmeldet
+//                    System.out.println(
+//                        "[Server] Client verbunden: "
+//                            + remoteClient
+//                                .getRemoteSocketAddress()); // damit wir sehen, woher Client sich
+//                    //                                 verbunden hat
+//                    // hier wird Verbindung dann verarbeitet
+//
+//
+//                    System.out.println("Remote-Client INet-Adresse" + remoteClient.getInetAddress());
+//                    System.out.println("Remote-Client Remote Socket Address"+remoteClient.getRemoteSocketAddress());
+//                    System.out.println("Remote-Client LocalSocketAddress"+remoteClient.getLocalSocketAddress());
+//                    System.out.println("Remote-Client Channel"+remoteClient.getChannel());
+//
+//                    // Input, OutputStream aus anderem Programm besser, nur zum Verständnis
+//                    Scanner scanner =
+//                        new Scanner(
+//                            new BufferedReader(
+//                                new InputStreamReader(remoteClient.getInputStream())));
+//                    if (scanner.hasNextLine()) {
+//                      System.out.println("[Server] Message from Client: " + scanner.nextLine());
+//                    }
+//
+//                    // Anfrage an Model weitergeben
+//                    // Antwort an Client zurückgeben
+//
+//                    // Verbindung danach wieder schließen
+//                    scanner.close();
+//                    remoteClient.close();
+//                    serverSocket.close();
+//
+//                  } catch (IOException e) {
+//                    e.printStackTrace();
+//                  }
+//                }
+//              }
+//            }).start();
+//  }
+
+
+
+
+
 }
