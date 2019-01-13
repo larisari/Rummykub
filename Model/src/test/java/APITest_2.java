cd94b4dcde43995e0cda4b537cd6221e9d20a9c7
yoshi21 <y.yacoub@campus.lmu.de>
2019-01-13 21:55:49 +0100

    removed getCombo method

--- Model/src/test/java/APITest_2.java
+++ Model/src/test/java/APITest_2.java
@@ -1,12 +1,29 @@
 import gameinfo.GIGameInfo;
+import gameinfo.GIFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
 
 public class APITest_2 {
   private static GIGameInfo gameInfo;
 
-  private static String player1_id;
-  private static String player2_id;
-  private static String player3_id;
-  private static String player4_id;
+  private static String player_1_id = UUID.randomUUID().toString();
+  private static String player_2_id = UUID.randomUUID().toString();
+  private static String player_3_id = UUID.randomUUID().toString();
+  private static String player_4_id = UUID.randomUUID().toString();
 
+
+  @BeforeAll
+  void setupGameInfo() {
+    gameInfo = GIFactory.make();
+    gameInfo.registerBy(player_1_id);
+    gameInfo.registerBy(player_2_id);
+    gameInfo.registerBy(player_3_id);
+    gameInfo.registerBy(player_4_id);
+  }
+
+  @Test
+  void
 
 }
