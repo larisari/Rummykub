8048eb18195e4c4974b252969bc5a75879443f35
jflxb <j.blenninger@campus.lmu.de>
2019-01-16 00:08:41 +0100

    refactored GITile to work with GINumber and GIColor rather then int's and GIColor.

--- Model/src/main/java/gameinfo/util/GITile.java
+++ Model/src/main/java/gameinfo/util/GITile.java
@@ -1,39 +1,22 @@
 package gameinfo.util;
 
 public class GITile {
-  private final int MIN_NUM = 1;
-  private final int MAX_NUM = 13;
-  private final int JOKER = 0;
   private GINumber number;
   private GIColor color;
 
   /**
-   * @throws IllegalArgumentException if:
-   * number not between 0 and 13
-   * joker declaration fails
-   * */
-  public GITile(GINumber number, GIColor color) {
-    if (number.value() >= MIN_NUM && number.value() <= MAX_NUM || number.value() == JOKER) {
-      this.number = number;
-      this.color = color;
-      checkForJoker();
-    }
-    else {
-      throw new IllegalArgumentException("Invalid argument for gameinfo.util.GITile " +
-          "declaration.");
-    }
+   * @throws IllegalArgumentException if Joker is created but either number or color is not of type
+   *     JOKER.
+   */
+  public GITile(GINumber number, GIColor color) throws IllegalArgumentException {
+    this.number = number;
+    this.color = color;
+    checkForJoker();
   }
 
-  private void checkForJoker() {
-    if (this.number == GINumber.JOKER) {
-      if (!this.color.equals(GIColor.JOKER)) {
-        throw new IllegalArgumentException("Joker must have color JOKER.");
-      }
-    }
-    if (this.color.equals(GIColor.JOKER)) {
-      if (this.number != GINumber.JOKER) {
-        throw new IllegalArgumentException("Joker must have number JOKER.");
-      }
+  private void checkForJoker() throws IllegalArgumentException {
+    if (!(number.equals(GINumber.JOKER) && color.equals(GIColor.JOKER))) {
+      throw new IllegalArgumentException("Joker must have GIColor JOKER and GINumber JOKER");
     }
   }
 
@@ -44,5 +27,4 @@ public class GITile {
   public GIColor getColor() {
     return this.color;
   }
-
 }
