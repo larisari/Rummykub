0d557cd3202a4d5ec7c24ad1b1153a795d988bae
markus <markus.franz@campus.lmu.de>
2019-01-16 16:44:51 +0100

    added testclasses and made server testruns

---
+++ ServerMerge/TestClient.java
@@ -0,0 +1,25 @@
+public class TestClient {
+  public static void main(String[] args) {
+    //
+
+
+
+
+          Client client0 = new Client("localhost");
+//          Client client1 = new Client("localhost");
+//          Client client2 = new Client("localhost");
+//          Client client3 = new Client("localhost");
+
+
+
+          client0.sendMessageToServer("gibStein");
+
+//          client1.sendMessageToServer("Der zweite Client is auch dabei!");
+//        client1.sendMessageToServer("NUmmer 3 ! BOCK ZU SPIELEN!");
+//      client1.sendMessageToServer("last one...");
+
+
+
+
+  }
+}
---
+++ ServerMerge/TestServer.java
@@ -0,0 +1,10 @@
+public class TestServer {
+
+  public static void main(String[] args) {
+
+
+      Server server = new Server();
+
+
+  }
+}
