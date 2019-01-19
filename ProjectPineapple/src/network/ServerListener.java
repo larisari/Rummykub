6e599488e83ddc8f17919a7b2559aaf42927b33e
Phil_Atom <p.greiner@campus.lmu.de>
2019-01-19 17:22:51 +0100

    added functions to Parsers

--- ProjectPineapple/src/network/ServerListener.java
+++ ProjectPineapple/src/network/ServerListener.java
@@ -50,13 +50,12 @@ public class ServerListener extends Thread {
             // Invoking the start() method
             t.start();
 
-            //Nachricht an Host Client, um Start Button zu aktivieren
-            if(getClients().size() > 2) {
-              clients.get(0).sendMessageToClient("possibleToStart");
-            }
-
             clients.add(t);
             Server.gameInfo.registerBy(clientID);
+            //Nachricht an Host Client, um Start Button zu aktivieren
+            if(clients.size() >= 2) {
+              clients.get(0).sendMessageToClient("possibleToStart");
+            }
             clientID++;
           } else {
             isRunning = false;
