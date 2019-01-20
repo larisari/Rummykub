480254bc8441607e006f825aad96663bb34a6be9
Larifari <s.sridharan@campus.lmu.de>
2019-01-20 16:52:34 +0100

    minor changes

--- ProjectPineapple/src/gui/TileView.java
+++ ProjectPineapple/src/gui/TileView.java
@@ -24,28 +24,6 @@ public class TileView {
   /**
    *
    */
-  List<Image> createImgs(String hand) {
-    List<Image> tilesImg = new ArrayList<>();
-    String[] block = hand.split(":");
-    String[] tiles = block[1].split(",");
-    for (int i = 0; i < tiles.length; i++) {
-      String[] attributeswS = tiles[i].split("[.]");
-      String[] attributes = attributeswS[1].split("/");
-      String color = attributes[0];
-      String number = attributes[1];
-
-      if (color.equals("joker")) {
-        Image joker = new Image("gui/images/tiles/joker/joker.png");
-        tilesImg.add(joker);
-      } else {
-        Image tile = new Image("gui/images/tiles/" + color + "/" + number + ".png");
-        tilesImg.add(tile);
-      }
-
-
-    }
-    return tilesImg;
-    }
 
 
 
