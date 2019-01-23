dfa1c7de17983368bff800a9b850b12d80afe76f
Phil_Atom <p.greiner@campus.lmu.de>
2019-01-23 19:48:53 +0100

    added function to parsers

--- ProjectPineG/src/main/java/network/ClientParser.java
+++ ProjectPineG/src/main/java/network/ClientParser.java
@@ -216,7 +216,6 @@ public class ClientParser {
         } else if (messageAsArray[1].equals("false")){
           Platform.runLater(() -> guiController.cancelSelEffect());
         }
-
         break;
 
       case "responseForPlaySwapJoker":
