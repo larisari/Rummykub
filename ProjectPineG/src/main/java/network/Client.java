dd243796280dba75e738bb8ec76a636798244032
markus <markus.franz@campus.lmu.de>
2019-01-23 15:40:27 +0100

    Javadoc added to client class.

--- ProjectPineG/src/main/java/network/Client.java
+++ ProjectPineG/src/main/java/network/Client.java
@@ -10,38 +10,36 @@ public class Client extends Thread {
 
   static DataInputStream in;
   static DataOutputStream out;
+  private static Logger log = Logger.getLogger(Client.class.getName());
   private final int PORT = 48410;
   Thread listeningThread;
   Socket socket;
   private String ip;
-  private static Logger log = Logger.getLogger(Client.class.getName());
 
+  //  /**
+  //   * Client-constructor for Hoster
+  //   */
+  //  public Client() {
+  //
+  //    try {
+  //      this.socket = new Socket("127.0.0.1", PORT);
+  //
+  //      in = new DataInputStream(socket.getInputStream());
+  //      out = new DataOutputStream(socket.getOutputStream());
+  //      this.startListening();
+  //
+  //
+  //      log.info("[Host-Client] wurde erstellt. In-/Outputstreams geöffnet. ");
+  //    } catch (IOException e) {
+  //      e.printStackTrace();
+  //    }
+  //  }
 
-//  /**
-//   * Client-constructor for Hoster
-//   */
-//  public Client() {
-//
-//    try {
-//      this.socket = new Socket("127.0.0.1", PORT);
-//
-//      in = new DataInputStream(socket.getInputStream());
-//      out = new DataOutputStream(socket.getOutputStream());
-//      this.startListening();
-//
-//
-//      log.info("[Host-Client] wurde erstellt. In-/Outputstreams geöffnet. ");
-//    } catch (IOException e) {
-//      e.printStackTrace();
-//    }
-//  }
-
-    /**
-     * Constructor to generate a Client, which connects to the Server.
-     * @param ip address of the server, a client wants to connect to.
-     */
-
-
+  /**
+   * Constructor to generate a Client, which connects to the Server.
+   *
+   * @param ip address of the server, a client wants to connect to.
+   */
   public Client(String ip) {
     try {
       this.socket = new Socket(ip, PORT);
@@ -56,10 +54,11 @@ public class Client extends Thread {
     }
   }
 
-    /**
-     * Sends a message to the server.
-     * @param message message to send to the server.
-     */
+  /**
+   * Sends a message to the server.
+   *
+   * @param message message to send to the server.
+   */
   public static void sendMessageToServer(String message) {
     String messageToServer = message;
     try {
@@ -69,34 +68,35 @@ public class Client extends Thread {
     }
   }
 
-    /**
-     * Opens a Thread, which listens to incoming messages from the Server and hands over to the Client Parser.
-     */
+  /**
+   * Opens a Thread, which listens to incoming messages from the Server and hands over to the Client
+   * Parser.
+   */
   public void startListening() {
-    listeningThread = new Thread(new Runnable() {
-      @Override
-      public void run() {
-        boolean running = true;
-        while (running) {
-          try {
-            if (socket.isConnected()) {
-              DataInputStream in = new DataInputStream(socket.getInputStream());
-              String msg = in.readUTF();
+    listeningThread =
+        new Thread(
+            new Runnable() {
+              @Override
+              public void run() {
+                boolean running = true;
+                while (running) {
+                  try {
+                    if (socket.isConnected()) {
+                      DataInputStream in = new DataInputStream(socket.getInputStream());
+                      String msg = in.readUTF();
 
-              log.info("[Client] Incoming message from Server: " + msg);
-              ClientParser.parseForController(msg);
+                      log.info("[Client] Incoming message from Server: " + msg);
+                      ClientParser.parseForController(msg);
 
-            } else {
-              running = false;
-            }
-          } catch (IOException e) {
-            e.printStackTrace();
-          }
-        }
-      }
-    });
+                    } else {
+                      running = false;
+                    }
+                  } catch (IOException e) {
+                    e.printStackTrace();
+                  }
+                }
+              }
+            });
     listeningThread.start();
   }
 }
-
-
