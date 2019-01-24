d555d29010916277d3c34f8aa7607f779f730a28
jflxb <j.blenninger@campus.lmu.de>
2019-01-24 14:45:52 +0100

    uncommented remove old combination from board.

--- ProjectPineG/src/main/java/gameinfo/GameInfoImpl.java
+++ ProjectPineG/src/main/java/gameinfo/GameInfoImpl.java
@@ -239,7 +239,7 @@ class GameInfoImpl extends Thread implements GIGameInfo {
       if (allValid) {
         player.remove(tilesFromHand);
         player.madeMove();
-        //board.remove(tilesFromBoard);
+        board.remove(tilesFromBoard);
         newCombinations.forEach(combination -> board.addCombo(combination));
         return Optional.of(new GITuple<>(id, true));
       } else {
