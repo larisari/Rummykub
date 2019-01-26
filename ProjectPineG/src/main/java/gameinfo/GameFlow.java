782a835818595c68a8586ca91fc8c98de061ec3a
yoshi21 <y.yacoub@campus.lmu.de>
2019-01-26 16:16:54 +0100

    JavaDoc

--- ProjectPineG/src/main/java/gameinfo/GameFlow.java
+++ ProjectPineG/src/main/java/gameinfo/GameFlow.java
@@ -1,8 +1,11 @@
+/**
+ * This class defines the game's general rythm. It explains the player's turn
+ * sequence, keeps track of all players as well as the game's progress and
+ * current state.
+ */
 package gameinfo;
-
 import gameinfo.util.GIPoints;
 import gameinfo.util.GITuple;
-
 import java.util.*;
 
 class GameFlow {
@@ -13,6 +16,13 @@ class GameFlow {
   private Integer currentPlayerIndex;
   private PointsCalculator calculator;
 
+  /**
+   * The one and only constructor of an GameFlow object only gets a
+   * PointsCalculator object as parameter, whereas all other attributes are
+   * not set manually by creating this object.
+   *
+   * @param calculator to calculate possible points.
+   */
   GameFlow(PointsCalculator calculator) {
     this.players = new HashMap<>();
     this.state = GameState.distributing;
@@ -22,34 +32,74 @@ class GameFlow {
     this.currentPlayerIndex = 0;
   }
 
+  /**
+   * Method for signing in players according to their age. Each player is
+   * assigned a unique ID, which is given as parameter to this method. On the
+   * basis of the ID, the player is put into a HashMap with his ID as
+   * key-value.
+   *
+   * @param id of the signed in player.
+   */
   void registerBy(Integer id) {
     players.put(id, new Player(id, calculator));
-    //order by age
     playerSequence.add(id);
     Collections.sort(playerSequence);
   }
 
+  /**
+   * Each time a player signs out, this method has to be called with the
+   * player's unique ID, to remove this player from the game.
+   *
+   * @param id of the player that wants to sign out of the game.
+   */
   void deregisterBy(Integer id) {
     players.remove(id);
     playerSequence.remove(id);
   }
 
+  /**
+   * A simple method to check, whether a player exists or not.
+   *
+   * @param id of the player that is being checked to exist.
+   * @return true, if player with given id exists and false otherwise
+   */
   boolean isPlayerExistingBy(Integer id) {
     return getPlayerBy(id).isPresent();
   }
 
+  /**
+   * This method causes the game's state to change to "distributing". In this
+   * state, each player can draw stacks from the bag.
+   */
   void startGame() {
     this.state = GameState.distributing;
   }
 
+  /**
+   * Simple method to check whether a player with given id is valid, in other
+   *  words, whether the player with given id has the correct id.
+   * @param id of the player that is being checked for validation.
+   * @return true, if player is valid, and false otherwise.
+   */
   boolean isValidPlayerBy(Integer id) {
     return players.get(id).getId().equals(id);
   }
 
+  /**
+   * This method returns all players in the game.
+   * @return all signed in players.
+   */
   Map<Integer, Player> getPlayers() {
     return this.players;
   }
 
+  /**
+   * Accessing a Player object with given id is possible with this method.
+   * @param id of the player.
+   * @return an Optional Player object, in case the given id matches a
+   * key-value in the HashMap with a valid value and an empty Optional if no
+   * player is found.
+   */
   Optional<Player> getPlayerBy(Integer id) {
     if (players.containsKey(id)) {
       return Optional.of(players.get(id));
@@ -58,10 +108,18 @@ class GameFlow {
     }
   }
 
+  /**
+   * This method is used to
+   * @return the current player's id.
+   */
   Integer getCurrentPlayerId() {
     return playerSequence.get(currentPlayerIndex);
   }
 
+  /**
+   * This method is used to
+   * @return the next player's id.
+   */
   Integer getNextPlayerID() {
     if (currentPlayerIndex < playerSequence.size() - 1) {
       return playerSequence.get(currentPlayerIndex + 1);
@@ -70,24 +128,40 @@ class GameFlow {
     }
   }
 
+  /**
+   * This method causes the next player's turn.
+   */
   void nextPlayersTurn() {
     if (currentPlayerIndex < playerSequence.size() - 1) {
       currentPlayerIndex++;
     } else {
       currentPlayerIndex = 0;
     }
-    Integer currentPlayerId  = playerSequence.get(currentPlayerIndex);
+    Integer currentPlayerId = playerSequence.get(currentPlayerIndex);
     players.get(currentPlayerId).resetMadeMove();
   }
 
+  /**
+   * Simple method to check whether the current state of the game is
+   * "distributing", in other words, whether stacks are being distributed.
+   * @return true, if game is in distributing-state, and false otherwise.
+   */
   boolean isDistributing() {
     return state.equals(GameState.distributing);
   }
 
+  /**
+   * This method is used to
+   * @return the number of players in the game.
+   */
   int getNumberOfPlayers() {
     return this.players.size();
   }
 
+  /**
+   * Causing the distributionCounter of the class to increase. Reaching the
+   * number of players, the state will change from "distributing" to "running".
+   */
   void addDistribution() {
     this.distributionCounter++;
     if (distributionCounter == getNumberOfPlayers()) {
@@ -95,6 +169,10 @@ class GameFlow {
     }
   }
 
+  /**
+   * Accessing the players' points is possible with this method.
+   * @return each players hands and their points.
+   */
   Optional<List<GITuple<Integer, GIPoints>>> getPlayerPoints() {
     if (!players.isEmpty()) {
       List<GITuple<Integer, GIPoints>> allPlayersHands = new ArrayList<>();
@@ -109,6 +187,11 @@ class GameFlow {
     }
   }
 
+  /**
+   * This method let's you know, whether a player has made a move.
+   * @param id of the player that possibly made a move.
+   * @return true, if player absolved a move successfully, and false otherwise.
+   */
   boolean hasMadeMoveBy(Integer id) {
     return players.get(id).hasMadeMove();
   }
