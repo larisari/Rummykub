e999ae9fb8a661855c32a8390b3a1d41957d7001
yoshi21 <y.yacoub@campus.lmu.de>
2019-01-31 00:08:44 +0100

    deciding between testing strategies

--- ProjectPineG/src/main/java/gameinfo/GameInfo_Test.java
+++ ProjectPineG/src/main/java/gameinfo/GameInfo_Test.java
@@ -4,26 +4,26 @@ import gameinfo.util.GIColor;
 import gameinfo.util.GINumber;
 import gameinfo.util.GITile;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.awt.image.TileObserver;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 class GameInfo_Test {
 
-  private static GIGameInfo gameInfo;
-
-  private static Player p1;
-  private static Player p2;
-  private static Player p3;
-  private static Player p4;
+  private static GIGameInfo gameInfo = GIFactory.make();
 
   private static final Integer player_1_ID = 1;
   private static final Integer player_2_ID = 2;
   private static final Integer player_3_ID = 3;
   private static final Integer player_4_ID = 4;
+
+  private static Player p1 = new Player(player_1_ID, new PointsCalculator());
+  private static Player p2 = new Player(player_2_ID, new PointsCalculator());
+  private static Player p3 = new Player(player_3_ID, new PointsCalculator());
+  private static Player p4 = new Player(player_4_ID, new PointsCalculator());
 
   private static List<GITile> hand_player_1 =
       new ArrayList<>(
@@ -45,65 +45,104 @@ class GameInfo_Test {
 
   @BeforeAll
   static void setup() {
-    gameInfo = GIFactory.make();
-    p1 = new Player(player_1_ID, new PointsCalculator());
-    p2 = new Player(player_2_ID, new PointsCalculator());
-    p3 = new Player(player_3_ID, new PointsCalculator());
-    p4 = new Player(player_4_ID, new PointsCalculator());
-  }
-
-  @Test
-  void fromZeroToDrawing() {
-    signInPlayers();
-    start();
-    draw();
-  }
-
-  @Test
-  void ageInfluence() {
-    signInPlayers();
-    setAge();
-    start();
-
-    assert gameInfo.getStartingPlayerId() == 4;
-    assert gameInfo.getCurrentPlayerId() == 4;
-    assert gameInfo.getNextPlayerId().get() == 3;
-  }
-
-  @Test
-  void invalidFirstMove() {
-    signInPlayers();
-    start();
-    draw();
-
-    List<GITile> playersGroup =
-        new ArrayList<>(
-            Arrays.asList(
-                gameInfo.getAllTilesBy(player_1_ID).get().getSecond().get(0),
-                gameInfo.getAllTilesBy(player_1_ID).get().getSecond().get(1),
-                gameInfo.getAllTilesBy(player_1_ID).get().getSecond().get(2)));
-
-    List<List<GITile>> combinations = new ArrayList<>();
-
-    combinations.add(playersGroup);
-
-    //Since the wished combination does not exceed the minimum of 30 points, the move is invalid.
-    assert !gameInfo.play(combinations, player_1_ID).get().getSecond();
-  }
-
-  void start() {
-    gameInfo.startGame();
-  }
-
-  void signInPlayers() {
     gameInfo.registerBy(player_1_ID);
     gameInfo.registerBy(player_2_ID);
     gameInfo.registerBy(player_3_ID);
     gameInfo.registerBy(player_4_ID);
   }
 
-  void setAge() {
+  //start each test with a new factory? -> one big test or several small? both?
+  //rather cover all methods or test game functionality?
 
+  @Test
+  void orderedTestRun() {
+    setAge();
+    start();
+    assert gameInfo.getStartingPlayerId().equals(player_4_ID);
+    assert gameInfo.getNextPlayerId().get().equals(player_3_ID);
+    draw();
+    //make moves and test game?
+  }
+
+  //-----------------------------------------------------------------------------
+  //several tests for small api-methods
+
+//  @Test
+//  void fromZeroToDrawing() {
+//    start();
+//
+//    assert gameInfo.getAllTilesBy(player_1_ID).get().getSecond().isEmpty();
+//    assert gameInfo.getAllTilesBy(player_2_ID).get().getSecond().isEmpty();
+//    assert gameInfo.getAllTilesBy(player_3_ID).get().getSecond().isEmpty();
+//    assert gameInfo.getAllTilesBy(player_4_ID).get().getSecond().isEmpty();
+//
+//    draw();
+//
+//    assert gameInfo.getAllTilesBy(player_1_ID).get().getSecond().size() == 14;
+//    assert gameInfo.getAllTilesBy(player_2_ID).get().getSecond().size() == 14;
+//    assert gameInfo.getAllTilesBy(player_3_ID).get().getSecond().size() == 14;
+//    assert gameInfo.getAllTilesBy(player_4_ID).get().getSecond().size() == 14;
+//  }
+//
+//  @Test
+//  void ageInfluence() {
+//    setAge();
+//    start();
+//
+//    assert gameInfo.getStartingPlayerId() == 4;
+//    assert gameInfo.getCurrentPlayerId() == 4;
+//    assert gameInfo.getNextPlayerId().get() == 3;
+//  }
+//
+//  @Test
+//  void invalidFirstCombination() {
+//
+//    start();
+//    draw();
+//
+//    List<GITile> playersGroup =
+//        new ArrayList<>(
+//            Arrays.asList(
+//                gameInfo.getAllTilesBy(player_1_ID).get().getSecond().get(0),
+//                gameInfo.getAllTilesBy(player_1_ID).get().getSecond().get(1),
+//                gameInfo.getAllTilesBy(player_1_ID).get().getSecond().get(2)));
+//
+//    List<List<GITile>> combinations = new ArrayList<>();
+//
+//    combinations.add(playersGroup);
+//
+//    //Since the wished combination does not exceed the minimum of 30 points, the move is invalid.
+//    assert !gameInfo.play(combinations, player_1_ID).get().getSecond();
+//  }
+//
+//  @Test
+//  void validFirstCombination() {
+//
+//    start();
+//    draw();
+//
+//    //no age is set, therefore the order goes according to the players' ids.
+//    assert gameInfo.getStartingPlayerId() == 1;
+//
+//    List<GITile> playersGroup =
+//        new ArrayList<>(
+//            Arrays.asList(
+//                gameInfo.getAllTilesBy(player_1_ID).get().getSecond().get(9),
+//                gameInfo.getAllTilesBy(player_1_ID).get().getSecond().get(10),
+//                gameInfo.getAllTilesBy(player_1_ID).get().getSecond().get(11)));
+//
+//    List<List<GITile>> combinations = new ArrayList<>();
+//    combinations.add(playersGroup);
+//
+//    //The combination exceeds the minimum of 30 points for the first move.
+//    assert gameInfo.play(combinations, player_1_ID).get().getSecond();
+//  }
+
+  void start() {
+    gameInfo.startGame();
+  }
+
+  void setAge() {
     gameInfo.setAgeFor(player_1_ID, 4);
     gameInfo.setAgeFor(player_2_ID, 3);
     gameInfo.setAgeFor(player_3_ID, 2);
@@ -111,78 +150,10 @@ class GameInfo_Test {
   }
 
   void draw() {
-    assert gameInfo.getAllTilesBy(player_1_ID).get().getSecond().isEmpty();
-    assert gameInfo.getAllTilesBy(player_2_ID).get().getSecond().isEmpty();
-    assert gameInfo.getAllTilesBy(player_3_ID).get().getSecond().isEmpty();
-    assert gameInfo.getAllTilesBy(player_4_ID).get().getSecond().isEmpty();
-
     gameInfo.drawBy(player_1_ID, hand_player_1);
     gameInfo.drawBy(player_2_ID);
     gameInfo.drawBy(player_3_ID);
     gameInfo.drawBy(player_4_ID);
-
-    assert gameInfo.getAllTilesBy(player_1_ID).get().getSecond().size() == 14;
-    assert gameInfo.getAllTilesBy(player_2_ID).get().getSecond().size() == 14;
-    assert gameInfo.getAllTilesBy(player_3_ID).get().getSecond().size() == 14;
-    assert gameInfo.getAllTilesBy(player_4_ID).get().getSecond().size() == 14;
   }
 
-  void checkGroup() {
-    List<GITile> street = new ArrayList<>();
-    street.add(hand_player_1.get(5));
-    street.add(hand_player_1.get(8));
-    street.add(hand_player_1.get(12));
-
-    List<GITile> elevens = new ArrayList<>();
-    elevens.add(hand_player_1.get(9));
-    elevens.add(hand_player_1.get(10));
-    elevens.add(hand_player_1.get(11));
-
-    List<List<GITile>> combos = new ArrayList<>();
-    combos.add(street);
-    combos.add(elevens);
-
-    System.out.println(gameInfo.play(combos, player_1_ID));
-  }
-
-  void checkOrder() {
-    assert gameInfo.getStartingPlayerId().equals(player_4_ID);
-    assert gameInfo.getCurrentPlayerId().equals(player_4_ID);
-    assert gameInfo.getNextPlayerId().get().equals(player_3_ID);
-  }
-
-  void validCombo() {
-    List<GITile> playersGroup =
-        new ArrayList<>(
-            Arrays.asList(
-                gameInfo.getAllTilesBy(player_1_ID).get().getSecond().get(9),
-                gameInfo.getAllTilesBy(player_1_ID).get().getSecond().get(10),
-                gameInfo.getAllTilesBy(player_1_ID).get().getSecond().get(11)));
-
-    List<List<GITile>> combinations = new ArrayList<>();
-    combinations.add(playersGroup);
-
-    assert gameInfo.play(combinations, player_1_ID).get().getSecond();
-  }
-
-  void getAllPlayersHands() {
-    System.out.println(gameInfo.calculatePointsForRegisteredPlayers());
-  }
-
-  void showhands() {
-    System.out.println(gameInfo.getAllTilesBy(player_1_ID).get());
-    System.out.println(gameInfo.getAllTilesBy(player_2_ID).get());
-    System.out.println(gameInfo.getAllTilesBy(player_3_ID).get());
-    System.out.println(gameInfo.getAllTilesBy(player_4_ID).get());
-
-    // player playes 3 tiles, so he should now have 11 left on his hand
-    assert gameInfo.getAllTilesBy(player_1_ID).get().getSecond().size() == 11;
-  }
-
-  void makeFirstMove() {
-    assert gameInfo.getNextPlayerId().get().equals(player_2_ID);
-    gameInfo.drawBy(player_1_ID);
-    assert gameInfo.getNextPlayerId().get().equals(player_3_ID);
-    assert gameInfo.getAllTilesBy(player_1_ID).get().getSecond().size() == 15;
-  }
 }
