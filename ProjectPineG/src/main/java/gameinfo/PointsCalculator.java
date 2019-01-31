7fd50fb6fbfacabc8dc76c324c72a29fd1f208ff
jflxb <j.blenninger@campus.lmu.de>
2019-01-31 21:37:12 +0100

    added JavaDoc to PointsCalculator

--- ProjectPineG/src/main/java/gameinfo/PointsCalculator.java
+++ ProjectPineG/src/main/java/gameinfo/PointsCalculator.java
@@ -5,6 +5,13 @@ import gameinfo.util.GITile;
 
 import java.util.List;
 
+/**
+ * This class provides methods to calculate the points for a street, group or any list of tiles. To
+ * be sure the points are calculated correctly only valid combinations should be passed with the
+ * methods. To sum all the tiles in a list use the method calculatePointsForHand.
+ *
+ * <p>The joker takes over the value of the stone he replaces in the combination.
+ */
 class PointsCalculator {
 
   PointsCalculator() {}
